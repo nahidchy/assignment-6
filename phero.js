031a74602fcf86612631412af16b16ac81dd7594
nahidchy <nicnahidchowdhury@gmail.com>
2024-05-14 15:46:45 +0600

    update

--- phero.js
+++ phero.js
@@ -8,7 +8,7 @@ const displayCategory=(allCategory)=>{
    const allCategoryContainer = document.getElementById("category-container");
    allCategory.forEach(category => {
        const button = document.createElement("button");
-       button.classList.add('text-xl','font-bold','p-3','active:bg-red-500', 'rounded', 'bg-slate-300');
+       button.classList.add('text-xl','font-bold','px-5','py-2','active:bg-red-500', 'rounded', 'bg-slate-300');
        button.setAttribute('onclick', `displayCategories("${category.category_id}")`);
        button.innerText = category.category;
        allCategoryContainer.append(button)
@@ -29,11 +29,18 @@ const displayCategories =async(id)=>{
             const div = document.createElement("div");
             div.innerHTML=`
              <img class="w-full px-3 lg:px-0 lg:w-64 h-40 rounded" src="${data.thumbnail}">
-             <div class="flex justify-start items-center  gap-4 mt-5">
+             <div class="flex justify-center  gap-4 mt-5">
              <img class="w-10 h-10 rounded-full" src="${data.authors[0].profile_picture}">
-             <div>
+             <div class="space-y-2">
+           
              <h4 class="text-xl font-medium mt-2">${data.title}</h4>
+             
+             <div class="flex items-center justify-between">
              <p>${data.authors[0].profile_name}</p>
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 bg-blue-700 text-white font-bold rounded-full">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+</svg>
+</div>
              <p>${data.others.views} views</p>
              </div>
              </div>
